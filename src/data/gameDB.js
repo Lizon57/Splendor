2d144c86518e67ecf7349702310119461a28d67d
O Y <orenyaniv90@gmail.com>
2022-03-28 09:41:43 +0300

    Update dummy game json

--- src/data/gameDB.js
+++ src/data/gameDB.js
@@ -86,10 +86,11 @@ export const gameDB = [
                     ],
                 }
             },
-            noble: {
-                stack: nobleCardDB,
-                shownCards: []
-            }
+            nobles: [
+                { cost: { emerald: 3, sapphire: 3, ruby: 0, diamond: 3, onyx: 0 } },
+                { cost: { emerald: 3, sapphire: 3, ruby: 3, diamond: 0, onyx: 0 } },
+                { cost: { emerald: 0, sapphire: 0, ruby: 3, diamond: 3, onyx: 3 } }
+            ]
         },
 
         coinStack: {
